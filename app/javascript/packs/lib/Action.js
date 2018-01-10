24a79abad5160fb5baef98e769d492ffdec5dbce
Kamil Ciemniewski <kamil@endpoint.com>
2018-01-10 16:20:24 +0000

    Made the simultaneous requests to the backend for the same resource grouped into just one abstract request

--- app/javascript/packs/lib/Action.js
+++ app/javascript/packs/lib/Action.js
@@ -2,14 +2,55 @@ import Selector from './Selector';
 import Request from './Request';
 
 export default class Action {
-    static run(state, options) {
-        let action = new this();
-        let selector = new Selector(action.constructor.name, options.select);
+    static get requests() {
+        if(Action._requests === undefined) {
+            Action._requests = new Map();
+        }
 
-        return action.execute(state, selector, options);
+        return Action._requests;
     }
 
-    get = Request.get;
+    static run(state, options) {
+        let action = new this();
+        action.selector = new Selector(action.constructor.name, options.select);
+
+        return action.execute(state, action.selector, options);
+    }
+
+    get(url, params) {
+        if(!Action.requests.has(this.selector.id)) {
+            Action.requests.set(this.selector.id, [ ]);
+
+            Request.get(url, params)
+                .then((data) => {
+                    Action.requests.get(this.selector.id).forEach((callback) => {
+                        callback(data, null);
+                    });
+                })
+                .catch((error) => {
+                    Action.requests.get(this.selector.id).forEach((callback) => {
+                        callback(null, error);
+                    });
+                })
+                .finally(() => {
+                    Action.requests.delete(this.selector.id);
+                });
+        }
+
+        return new Promise((resolve, reject) => {
+            let callbacks = Action.requests.get(this.selector.id);
+
+            callbacks.push((data, error) => {
+                if(data === null) {
+                    reject(error);
+                }
+                else {
+                    resolve(data);
+                }
+            });
+        });
+    }
+
     post = Request.post;
     put = Request.post;
 }
