def963abd05c9b4d1e1334fd8f0f3c23b126b0f4
Kamil Ciemniewski <kamil@endpoint.com>
2018-01-26 21:05:32 +0000

    More fixes

--- app/actions/graphemes/query_diff.rb
+++ app/actions/graphemes/query_diff.rb
@@ -5,38 +5,54 @@ module Graphemes
     validates :revision_right, presence: true
 
     def execute
-      sql = <<-sql
-        select g.grapheme_id as id,
-              g.inclusion[1],
-              graphemes.status,
-              graphemes.value,
-              graphemes.area,
-              graphemes.zone_id,
-            graphemes.position_weight,
-            graphemes.parent_ids,
-            surfaces.number as surface_number
-        from (
-          select gs.grapheme_id, array_agg(gs.inclusion) as inclusion
-          from (
-            select grapheme_id, 'left' as inclusion
-            from #{revision_left.graphemes_revisions_partition_table_name}
-            union all
-            select grapheme_id, 'right' as inclusion
-            from #{revision_right.graphemes_revisions_partition_table_name}
-          ) gs
-          group by grapheme_id
-          having array_length(array_agg(gs.inclusion), 1) < 2
-        ) g
-        inner join graphemes
-                on graphemes.id = g.grapheme_id
-        inner join zones
-                  on zones.id = graphemes.zone_id
-        inner join surfaces
-                on surfaces.id = zones.surface_id
-        order by surfaces.number, graphemes.position_weight
-      sql
+      all_diffs.reject { |g| has_mirrored?(g) }
+    end
 
-      Grapheme.find_by_sql sql
+    def all_diffs
+      @_all_diffs ||= -> {
+        sql = <<-sql
+          select g.grapheme_id as id,
+                g.inclusion[1],
+                graphemes.status,
+                graphemes.value,
+                graphemes.area,
+                graphemes.status,
+                graphemes.zone_id,
+              graphemes.position_weight,
+              graphemes.parent_ids,
+              surfaces.number as surface_number
+          from (
+            select gs.grapheme_id, array_agg(gs.inclusion) as inclusion
+            from (
+              select grapheme_id, 'left' as inclusion
+              from #{revision_left.graphemes_revisions_partition_table_name}
+              union all
+              select grapheme_id, 'right' as inclusion
+              from #{revision_right.graphemes_revisions_partition_table_name}
+            ) gs
+            group by grapheme_id
+            having array_length(array_agg(gs.inclusion), 1) < 2
+          ) g
+          inner join graphemes
+                  on graphemes.id = g.grapheme_id
+          inner join zones
+                    on zones.id = graphemes.zone_id
+          inner join surfaces
+                  on surfaces.id = zones.surface_id
+          order by surfaces.number, graphemes.position_weight
+        sql
+
+        Grapheme.find_by_sql sql
+      }.call
+    end
+
+    def has_mirrored?(grapheme)
+      all_diffs.any? do |other|
+        grapheme.inclusion != other.inclusion &&
+          grapheme.value == other.value &&
+          grapheme.area == other.area &&
+          grapheme.surface_number == other.surface_number
+      end
     end
   end
 end
