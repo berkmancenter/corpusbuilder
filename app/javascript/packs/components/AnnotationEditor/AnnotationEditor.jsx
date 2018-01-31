611edd113cc3e1e828a245d95ba73f3bbe429756
Kamil Ciemniewski <kamil@endpoint.com>
2018-01-31 13:11:18 +0000

    Coded the UI for structural tagging editions

--- app/javascript/packs/components/AnnotationEditor/AnnotationEditor.jsx
+++ app/javascript/packs/components/AnnotationEditor/AnnotationEditor.jsx
@@ -14,15 +14,66 @@ import styles from './AnnotationEditor.scss'
 @observer
 export default class AnnotationEditor extends React.Component {
 
-    textArea = null;
-
     @observable
     editedAnnotation = "";
+
+    @observable
+    isMainMenuOpened = false;
+
+    @observable
+    chosenMode = null;
+
+    @observable
+    modes = {
+        meta: true,
+        isOpen: false,
+        title: (() => { return this.currentMode.title }),
+        comment: { title: 'Comment' },
+        category: { title: 'Category' },
+        structural: {
+            meta: true,
+            isOpen: false,
+            title: 'Structure',
+            h1: { title: 'Header 1' },
+            h2: { title: 'Header 2' },
+            h3: { title: 'Header 3' },
+            h4: { title: 'Header 4' },
+            h5: { title: 'Header 5' },
+            p:  { title: 'Paragraph' }
+        },
+        biographical: {
+            meta: true,
+            isOpen: false,
+            title: 'Biography',
+            man: { title: 'Biography of a man' },
+            woman: { title: 'Biography of a woman' },
+            year_birth: { title: 'Year of birth' },
+            year_death: { title: 'Year of death' },
+            age: { title: 'Age in years' },
+            person: { title: 'Person' }
+        },
+        analytical: {
+            meta: true,
+            isOpen: false,
+            title: 'Analysis',
+            administrative: { title: 'Administrative division' },
+            route: { title: 'Route' }
+        }
+    }
+
+    @computed
+    get currentMode() {
+        return this.chosenMode || this.modes.comment;
+    }
 
     requestClose() {
         if(this.props.onCloseRequested !== undefined && this.props.onCloseRequested !== null) {
             this.props.onCloseRequested();
         }
+    }
+
+    chooseMode(mode) {
+        this.chosenMode = mode;
     }
 
     onClickedOutside() {
@@ -52,16 +103,76 @@ export default class AnnotationEditor extends React.Component {
         this.editedAnnotation = "";
     }
 
+    extractMenuItems(level) {
+        return Object.keys(level)
+                     .filter((key) => { return key !== 'meta' && key !== 'title' && key !== 'isOpen' })
+                     .map((key) => {
+                         let ret = {};
+
+                         ret[ key ] = level[ key ];
+
+                         return ret;
+                     });
+    }
+
+    renderMenu(menu = { root: this.modes }) {
+        let key = Object.keys(menu)[0];
+        let level = menu[ key ];
+        let toggle = ((on = false) => { level.isOpen = on }).bind(this);
+        let menuSpec = {
+            isOpen: level.isOpen === true,
+            close: toggle.bind(this, false),
+            align: 'left',
+            toggle: (
+              <Button toggles={ true }
+                      toggled={ level.isOpen === true }
+                      onToggle={ toggle.bind(this, !level.isOpen) }
+                      >
+                      { typeof level.title === "function" ? level.title() : level.title }
+              </Button>
+            )
+        };
+        let items = this.extractMenuItems(level);
+
+        if(level === this.modes) {
+            return (
+              <DropdownMenu {...menuSpec}>
+                  {
+                      items.map((item) => {
+                          return this.renderMenu(item)
+                      })
+                  }
+              </DropdownMenu>
+            );
+        }
+        else {
+            if(level.meta === true) {
+                return (
+                  <NestedDropdownMenu key={ key } {...menuSpec}>
+                      {
+                          items.map((item) => {
+                              return this.renderMenu(item)
+                          })
+                      }
+                  </NestedDropdownMenu>
+                );
+            }
+            else {
+                return (
+                    <li key={ key }>
+                        <button type="button" onClick={ this.chooseMode.bind(this, level) }>
+                            { level.title }
+                        </button>
+                    </li>
+                );
+            }
+        }
+    }
+
     render() {
         if(!this.props.visible) {
             return null;
         }
-
-        setTimeout(() => {
-            if(this.textArea !== null && this.textArea !== null) {
-                this.textArea.focus();
-            }
-        }, 100)
 
         return (
             <div>
@@ -69,12 +180,13 @@ export default class AnnotationEditor extends React.Component {
                                 onCloseRequested={ this.onClickedOutside.bind(this) }
                                 >
                   <div className="corpusbuilder-annotation-editor">
-                      <span>Annotate fragment:</span>
-                      <b>CTRL-Enter to save</b>
+                      <div className="corpusbuilder-annotation-editor-menu">
+                          { this.renderMenu() }
+                          <b>CTRL-Enter to save</b>
+                      </div>
                       <textarea onChange={ this.onAnnotationChanged.bind(this) }
                                 onKeyUp={ this.onEditorKeyUp.bind(this) }
                                 value={ this.editedAnnotation }
-                                ref={ (textArea) => this.textArea = textArea  }
                                 rows="5">
                       </textarea>
                       <div className="corpusbuilder-annotation-editor-buttons">
--- public/packs/application.js
+++ public/packs/application.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "/packs/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 492);
+/******/ 	return __webpack_require__(__webpack_require__.s = 497);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 492:
+/***/ 497:
 /*!*********************************************!*\
   !*** ./app/javascript/packs/application.js ***!
   \*********************************************/
