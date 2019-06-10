1471ead553c9b895d4be8516eea3f31fedea8231
Kamil Ciemniewski <kamil@endpoint.com>
2019-06-10 11:56:07 +0000

    WIP: worked on the ground truth data to document importer

---
+++ app/actions/documents/import/kraken_exported.rb
@@ -0,0 +1,19 @@
+module Documents
+  class Import::KrakenExported < Import::Base
+    def image_paths
+      memoized do
+
+      end
+    end
+
+    def metadata
+      memoized do
+
+      end
+    end
+
+    def elements_for(image)
+
+    end
+  end
+end
