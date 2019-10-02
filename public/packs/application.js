297f8b64c04fff0f4f3deafc8b65c4135a827506
Kamil Ciemniewski <kamil@endpoint.com>
2019-10-02 17:12:13 +0000

    Added webpacker compilation step

--- public/packs/application.js
+++ public/packs/application.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "/packs/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1236);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1239);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 1236:
+/***/ 1239:
 /*!*********************************************!*\
   !*** ./app/javascript/packs/application.js ***!
   \*********************************************/
